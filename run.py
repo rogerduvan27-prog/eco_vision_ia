26939406b7656d6ce94709b403b592665468749e
Roger Duvan <rogerduvan27@gmail.com>
2026-03-22 22:01:04 -0500

    Revertir cambios hosting

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mail import Mail
 from app.conexion import conexion_DB
